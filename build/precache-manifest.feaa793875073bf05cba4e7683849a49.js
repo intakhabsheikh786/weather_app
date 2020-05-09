46d6b300333df5676d85a32e75de3c2eecd16cd7
Intakhab Sheikh <intakhabsheikh786@gmail.com>
2020-05-09 23:09:53 +0530

    after build

---
+++ build/precache-manifest.feaa793875073bf05cba4e7683849a49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf761689c0f58bc2ad35f0ca7a7eb5c",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "68dc1da52cd11882bcb0",
+    "url": "/weather_app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6d8c1aadaf2ab29648d3",
+    "url": "/weather_app/static/css/main.fd1af548.chunk.css"
+  },
+  {
+    "revision": "68dc1da52cd11882bcb0",
+    "url": "/weather_app/static/js/2.b0bc1465.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather_app/static/js/2.b0bc1465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8c1aadaf2ab29648d3",
+    "url": "/weather_app/static/js/main.b3dc6381.chunk.js"
+  },
+  {
+    "revision": "51d620ab4a053ed9185a",
+    "url": "/weather_app/static/js/runtime-main.83f808b1.js"
+  },
+  {
+    "revision": "fd447e2f4a78628c7f81ceb569aac663",
+    "url": "/weather_app/static/media/loading.fd447e2f.gif"
+  },
+  {
+    "revision": "607f29a90d474b86f0fe0fbd0871561e",
+    "url": "/weather_app/static/media/logo.607f29a9.jpg"
+  }
+]);
